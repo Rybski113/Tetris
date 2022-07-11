e96fd9fe9e7db93d2f0e94e2852c614900022b63
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-11 20:06:41 +0200

    Future tetrominoes in mini-grid

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const scoreDisplay = document.querySelector('#score')
     const startBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
 
     const lTetromino = [
         [1, width+1, width*2+1, 2],
@@ -97,10 +98,12 @@ document.addEventListener('DOMContentLoaded', ()=> {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach (index => squares[currentPosition + index].classList.add('taken'))
 
-            random = Math.floor(Math.random() * theTetrominoes.length)
+            random = nextRandom
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
             current = theTetrominoes[random] [currentRotation]
             currentPosition = 4
             draw()
+            displayShape()
         }
     }
 
@@ -143,6 +146,27 @@ document.addEventListener('DOMContentLoaded', ()=> {
     }
 
 
+    const displaySquares = document.querySelectorAll('.mini-grid div')
+    const displayWidth = 4
+    let displayIndex = 0
+    
 
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2],
+        [displayWidth*2, displayWidth*2+1, displayWidth+1, displayWidth+2],
+        [1, displayWidth, displayWidth+1, displayWidth+2],
+        [0,1, displayWidth, displayWidth+1],
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1],
+    ]
+
+
+    function displayShape() {
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+        upNextTetrominoes[nextRandom].forEach( index => {
+            displaySquares[displayIndex + index].classList.add('tetromino')
+        })
+    }
 
 })
