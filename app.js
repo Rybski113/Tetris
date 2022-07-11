70ec6b28c5177d72d776fcbde6291839c825248d
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-11 18:39:16 +0200

    Blocks move right

--- app.js
+++ app.js
@@ -74,9 +74,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
        } else if (e.keyCode === 38) {
         //rotate()
        } else if(e.keyCode === 39) {
-        //moveRight()
+        moveRight()
        } else if(e.keyCode === 40) {
-        //moveDown()
+        moveDown()
        }
     }
 
@@ -116,4 +116,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
         draw()
     }
+
+    function moveRight() {
+       undraw()
+       const isAtRightEdge = current.some( index => (currentPosition + index) % width === width -1)
+
+       if(!isAtRightEdge) currentPosition +=1
+
+       if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1
+       }
+    }
+
+    draw()
 })
