6b31bf9842ee0c663e7cabe1a10e3ab449ae9093
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-11 17:59:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -67,6 +67,15 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     timerId = setInterval(moveDown, 300)
 
+
+    function control() {
+        
+    }
+
+
+
+
+
     function moveDown() {
         undraw()
         currentPosition += width
@@ -84,5 +93,18 @@ document.addEventListener('DOMContentLoaded', ()=> {
             currentPosition = 4
             draw()
         }
+    }
+
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some( index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+
+        draw()
     }
 })
