0d9e2814308abe06919f2ed94e0dc3995c922303
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-11 17:04:10 +0200

    Block stops on each other

--- app.js
+++ app.js
@@ -65,12 +65,24 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 
 
-    timerId = setInterval(moveDown, 1000)
+    timerId = setInterval(moveDown, 300)
 
     function moveDown() {
         undraw()
         currentPosition += width
         draw()
+        freeze()
 
+    }
+
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach (index => squares[currentPosition + index].classList.add('taken'))
+
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random] [currentRotation]
+            currentPosition = 4
+            draw()
+        }
     }
 })
